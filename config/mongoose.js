cb0d7b01c143cdf46c38a993ba3398d2e45f99c4
Tushar Bahuguna <tusharbahuguna2528@gmail.com>
2023-06-06 15:22:47 +0530

    Add env variables

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,7 +1,7 @@
 // **************************Database part with mongoose********************************
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017/userDB', {useNewUrlParser : true});
+mongoose.connect(process.env.MONGODB_URL );
 
 
 
